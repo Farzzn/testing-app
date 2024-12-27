c3614ab5f78854346feb704ff90a1ddd1c93564b
farhan <farhanmoozhikkal@gmail.com>
2024-12-27 11:08:20 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/testing-app", // Matches the repository name
   output: "export", // Ensures Next.js generates a static export
+  images: {
+    unoptimized: true // Required for static export
+  },
 };
 
 export default nextConfig;
